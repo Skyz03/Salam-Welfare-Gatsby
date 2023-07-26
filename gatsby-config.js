be6aabb8f89debd01a410e53b1c9e281195e05da
skyz03 <aakibdiscoo@gmail.com>
2023-07-26 15:44:09 +0545

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,26 @@ module.exports = {
     title: `salam-welfare-gatsby`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
     },
-    __key: "images"
-  }]
+    {
+      resolve: `gatsby-plugin-google-gtag`, options: {
+        trackingIds: [
+          "268730374"
+        ],
+      },
+    },
+    "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }]
 };
